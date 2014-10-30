dd935f8c39de022d3cbba66eb85a9215cdac1303
Andrew Davis <andrew@lightcorp.net>
2014-10-30 15:17:59 -0500

    update protractor tests to use localhost

--- test/protractor/modules/screenOwnership.js
+++ test/protractor/modules/screenOwnership.js
@@ -2,7 +2,7 @@
 describe('screen ownership is the first thing that loads', function() {
 
   it('and should have a user interface', function() {
-    browser.get('http://192.168.1.151:8080/');
+    browser.get('http://localhost:8080/');
 
     expect(element(by.id('add-identities')).isPresent()).toBe(true);
     expect(element(by.id('scan-barcode')).isPresent()).toBe(true);
