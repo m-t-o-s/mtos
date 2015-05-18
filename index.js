e2cca76d70b3e16a2e8e91a9ed02040772041071
Andrew Davis <diffalot@diff.mx>
2015-05-17 19:10:27 -0500

    content signed, encrypted, zipped, and seeded as torrent

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
 'use strict'
 
-var Buffer = require('buffer').Buffer
 var q = require('q')
+var JSZip = require('jszip')
 
 var mtos = {}
 
 mtos.torrentClient = require('./lib/webtorrent')
 mtos.crypter = require('./lib/crypter')
-
-mtos.createTextFile = function (text) {
-  var deferred = q.defer()
-  var buffer = new Buffer(Buffer.byteLength(text, 'utf-8'))
-  buffer.write(text, 'utf-8')
-  mtos.torrentClient.seed(buffer, { name: 'data' }, function (torrent) {
-    deferred.resolve(torrent)
-  })
-  return deferred.promise
-}
 
 mtos.readTextFile = function (torrent) {
   var deferred = q.defer()
@@ -27,6 +17,52 @@ mtos.readTextFile = function (torrent) {
     deferred.resolve(buffer.toString('utf-8'))
   })
   return deferred.promise
+}
+
+mtos.createZip = function (data) {
+  var deferred = q.defer()
+  var zip = new JSZip()
+  zip.file('mtos-data.json', data)
+  var zipfile = zip.generate({type: 'nodebuffer'})
+  deferred.resolve(zipfile)
+  return deferred.promise
+}
+
+mtos.signContent = mtos.crypter.signContent
+mtos.encryptContent = mtos.crypter.encryptContent
+
+mtos.createContent = function (content, options) {
+  console.log('creating', content, options)
+  var contentString = JSON.stringify(content)
+  return mtos.signContent(contentString, options.privateKey)
+  .then(function (signedContent) {
+    var signedContentString = JSON.stringify(signedContent)
+    if (options.encrypt) {
+      return mtos.encryptContent(signedContentString, options.publicKey)
+    } else {
+      var deferred = q.defer()
+      deferred.resolve(signedContentString)
+      return deferred.promise
+    }
+  })
+  .then(function (finalContent) {
+    var content = JSON.stringify({
+      message: finalContent,
+      author: options.author
+    })
+    console.log('about to zip', content)
+    return mtos.createZip(content)
+  })
+  .then(function (zipfile) {
+    var deferred = q.defer()
+    mtos.torrentClient.seed(zipfile, { name: 'mtos-data.zip' }, function (torrent) {
+      deferred.resolve(torrent)
+    })
+    return deferred.promise
+  })
+  .then(function (torrent) {
+    console.log('created torrent', torrent)
+  })
 }
 
 mtos.newServerKey = mtos.crypter.generateKeyPair
