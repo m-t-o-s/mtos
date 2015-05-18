d9c5039c2d7ba753109b22d7a9188905acfeb82d
Andrew Davis <diffalot@diff.mx>
2015-05-18 08:25:22 -0500

    removed signing, got encryption and decryption working with zip files stored as torrents

--- index.js
+++ index.js
@@ -22,9 +22,18 @@ mtos.readTextFile = function (torrent) {
 mtos.createZip = function (data) {
   var deferred = q.defer()
   var zip = new JSZip()
-  zip.file('mtos-data.json', data)
+  zip.file('mt-data.json', data)
   var zipfile = zip.generate({type: 'nodebuffer'})
   deferred.resolve(zipfile)
+  return deferred.promise
+}
+
+mtos.readZip = function (data) {
+  console.log('reading zip content', data)
+  var deferred = q.defer()
+  var zip = new JSZip(data)
+  var content = zip.file('mt-data.json').asText()
+  deferred.resolve(JSON.parse(content).message)
   return deferred.promise
 }
 
@@ -55,14 +64,47 @@ mtos.createContent = function (content, options) {
   })
   .then(function (zipfile) {
     var deferred = q.defer()
-    mtos.torrentClient.seed(zipfile, { name: 'mtos-data.zip' }, function (torrent) {
+    mtos.torrentClient.seed(zipfile, { name: 'mt-data.zip' }, function (torrent) {
       deferred.resolve(torrent)
     })
     return deferred.promise
   })
   .then(function (torrent) {
     console.log('created torrent', torrent)
+    return torrent
   })
+}
+
+mtos.decryptContent = mtos.crypter.decryptContent
+mtos.verifyContent = mtos.crypter.verifyContent
+
+mtos.readContent = function (torrent, options) {
+  return mtos.torrentToBuffer(torrent)
+  .then(function (mtZipBuffer) {
+    return mtos.readZip(mtZipBuffer)
+  })
+  .then(function (mtData) {
+    console.log('read from zip', mtData)
+    return mtos.decryptContent(mtData, options.privateKey)
+  })
+}
+
+mtos.torrentToBuffer = function (torrent) {
+  var deferred = q.defer()
+  console.log('beginning torrent read cycle', torrent)
+  var mtZip
+  for (var i = 0; i < torrent.files.length; i++) {
+    if (torrent.files[i].path === 'mt-data.zip') {
+      mtZip = torrent.files[i]
+    }
+  }
+  mtZip.getBuffer(function (error, buffer) {
+    if (error) {
+      throw new Error(error)
+    }
+    deferred.resolve(buffer)
+  })
+  return deferred.promise
 }
 
 mtos.newServerKey = mtos.crypter.generateKeyPair
