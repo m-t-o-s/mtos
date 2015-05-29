cba562e085ab169f2528862aaf56564e6dd914cd
Andrew Davis <diffalot@diff.mx>
2015-05-28 21:43:55 -0500

    better wording of tape test descriptions

--- test/basic.js
+++ test/basic.js
@@ -10,14 +10,14 @@ var serverKeyTwo = mtos.newServerKey()
 function ensureKey (key, t) {
   var fingerpringStringLength = key.publicKeyFingerprint.length
   t.ok(key.publicKey, 'has public key')
-  t.ok(key.publicKey.encrypt, 'has public key for encryption')
-  t.ok(key.publicKey.verify, 'has public key for verification')
+  t.ok(key.publicKey.encrypt, 'can use public key for encryption')
+  t.ok(key.publicKey.verify, 'can use public key for verification')
   t.ok(key.privateKey, 'has private key')
-  t.ok(key.privateKey.decrypt, 'has private key for decryption')
-  t.ok(key.privateKey.sign, 'has private key for signing')
+  t.ok(key.privateKey.decrypt, 'can use private key for decryption')
+  t.ok(key.privateKey.sign, 'can use private key for signing')
   t.ok(key.publicKeyString, 'has public key string')
   t.ok(key.privateKeyString, 'has private key string')
-  t.equal(59, fingerpringStringLength, 'has a fingerprint 59 characters long')
+  t.equal(59, fingerpringStringLength, 'has a public key fingerprint 59 characters long')
 }
 
 tape('generate a server key', function (t) {
@@ -34,7 +34,7 @@ tape('generate another server key', function (t) {
   })
 })
 
-tape('keys are not equal', function (t) {
+tape('server keys are not equal', function (t) {
   return q.all([serverKeyOne, serverKeyTwo])
   .then(function (keys) {
     t.notEqual(keys[0].publicKeyFingerprint, keys[1].publicKeyFingerprint, 'the keys have different fingerprints')
