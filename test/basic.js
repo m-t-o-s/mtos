0fc7f3c62bf14babe30f5b07514c10b1bca55c72
Andrew Davis <diffalot@diff.mx>
2015-05-28 20:32:13 -0500

    add tests that server key was generated successfully

--- test/basic.js
+++ test/basic.js
@@ -1,8 +1,21 @@
 var mtos = require('../')
-var tape = require('tape')
+var tape = require('blue-tape')
 
-tape('tape works', function (t) {
+tape('generate server key', function (t) {
 
-  t.equal(true, true)
+  return mtos.newServerKey()
+  .then(function (key) {
+    var fingerpringStringLength = key.publicKeyFingerprint.length
+    t.ok(key.publicKey, 'has public key')
+    t.ok(key.publicKey.encrypt, 'has public key for encryption')
+    t.ok(key.publicKey.verify, 'has public key for verification')
+    t.ok(key.privateKey, 'has private key')
+    t.ok(key.privateKey, 'has private key')
+    t.ok(key.privateKey.decrypt, 'has private key for decryption')
+    t.ok(key.privateKey.sign, 'has private key for signing')
+    t.ok(key.publicKeyString, 'has public key string')
+    t.ok(key.privateKeyString, 'has private key string')
+    t.equal(59, fingerpringStringLength, 'has a fingerprint 59 characters long')
+  })
 
 })
