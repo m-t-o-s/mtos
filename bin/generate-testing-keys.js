b64460bd2ec967611d7ef06859d50b37aeaedcb5
Andrew Davis <diffalot@diff.mx>
2015-05-29 09:19:58 -0500

    generate testing keys during postinstall

---
+++ bin/generate-testing-keys.js
@@ -0,0 +1,31 @@
+'use strict'
+
+var mtos = require('../')
+var q = require('q')
+var fs = require('fs')
+var path = require('path')
+
+var testingKeys = {}
+var serverKeyOne = mtos.newServerKey()
+var serverKeyTwo = mtos.newServerKey()
+
+console.log('generating testing keys')
+
+q.all([serverKeyOne, serverKeyTwo])
+.then(function (serverKeys) {
+  var deferred = q.defer()
+  delete serverKeys[0].privateKey
+  delete serverKeys[0].publicKey
+  delete serverKeys[1].privateKey
+  delete serverKeys[1].publicKey
+  testingKeys.serverKeyOne = serverKeys[0]
+  testingKeys.serverKeyTwo = serverKeys[1]
+  fs.writeFileSync(path.join(__dirname + '/../test/testing-keys.json'),
+                   JSON.stringify(testingKeys, null, 4), 'utf-8')
+  deferred.resolve(testingKeys)
+  return deferred.promise
+})
+.finally(function () {
+  console.log('generated testing keys')
+  process.exit()
+})
