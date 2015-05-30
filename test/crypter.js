95d2eeffa7a831186cabea2cd590254d5f332d52
Andrew Davis <diffalot@diff.mx>
2015-05-30 10:28:57 -0500

    remove q dependency, add process.exit() as a dirty way to end tape run

--- test/crypter.js
+++ test/crypter.js
@@ -2,7 +2,6 @@
 
 var mtos = require('../')
 var tape = require('tape')
-var q = require('q')
 
 var testingKeys = require('./testing-keys.json')
 
@@ -10,23 +9,24 @@ var serverKeyOne = mtos.loadKeyFromStrings(testingKeys.serverKeyOne)
 var serverKeyTwo = mtos.loadKeyFromStrings(testingKeys.serverKeyTwo)
 
 function ensureKey (key, t) {
-  var deferred = q.defer()
-  var fingerpringStringLength = key.publicKeyFingerprint.length
-  t.ok(key.publicKey, 'has public key')
-  t.ok(key.publicKey.encrypt, 'can use public key for encryption')
-  t.ok(key.publicKey.verify, 'can use public key for verification')
-  t.ok(key.privateKey, 'has private key')
-  t.ok(key.privateKey.decrypt, 'can use private key for decryption')
-  t.ok(key.privateKey.sign, 'can use private key for signing')
-  t.ok(key.publicKeyString, 'has public key string')
-  t.ok(key.privateKeyString, 'has private key string')
-  t.equal(59, fingerpringStringLength, 'has a public key fingerprint 59 characters long')
-  deferred.resolve(key)
-  return deferred.promise
+  var promise = new Promise(function (resolve, reject) {
+    var fingerpringStringLength = key.publicKeyFingerprint.length
+    t.ok(key.publicKey, 'has public key')
+    t.ok(key.publicKey.encrypt, 'can use public key for encryption')
+    t.ok(key.publicKey.verify, 'can use public key for verification')
+    t.ok(key.privateKey, 'has private key')
+    t.ok(key.privateKey.decrypt, 'can use private key for decryption')
+    t.ok(key.privateKey.sign, 'can use private key for signing')
+    t.ok(key.publicKeyString, 'has public key string')
+    t.ok(key.privateKeyString, 'has private key string')
+    t.equal(59, fingerpringStringLength, 'has a public key fingerprint 59 characters long')
+    resolve(key)
+  })
+  return promise
 }
 
 tape('keys loaded from strings are valid', function (t) {
-  return q.all([serverKeyOne, serverKeyTwo])
+  return Promise.all([serverKeyOne, serverKeyTwo])
   .then(function (keys) {
     for (var i = 0; i < keys.length; i++) {
       ensureKey(keys[i], t)
@@ -36,7 +36,7 @@ tape('keys loaded from strings are valid', function (t) {
 })
 
 tape('server keys are not equal', function (t) {
-  return q.all([serverKeyOne, serverKeyTwo])
+  return Promise.all([serverKeyOne, serverKeyTwo])
   .then(function (keys) {
     t.notEqual(keys[0].publicKeyFingerprint, keys[1].publicKeyFingerprint, 'the keys have different fingerprints')
     t.end()
@@ -48,7 +48,7 @@ tape('can encrypt from a public and decrypt from a private key', function (t) {
   var content = {
     secretMessage: 'there are secrets here'
   }
-  return q.all([serverKeyOne, serverKeyTwo])
+  return Promise.all([serverKeyOne, serverKeyTwo])
   .then(function (keys) {
     keypairs = keys
     return mtos.encryptContent(content, keypairs[1].publicKey)
@@ -59,5 +59,6 @@ tape('can encrypt from a public and decrypt from a private key', function (t) {
   .then(function (decryptedContent) {
     t.deepEqual(decryptedContent, content, 'decrypted secret message')
     t.end()
+    process.exit()
   })
 })
