7ef885693ed0b0d71d3176a7b1480570e041f6ee
Andrew Davis <diffalot@diff.mx>
2015-09-10 21:27:46 -0500

    generate user keys for tests

--- bin/generate-testing-keys.js
+++ bin/generate-testing-keys.js
@@ -8,17 +8,41 @@ var testingKeys = {}
 var serverKeyOne = mtos.generateServerKey()
 var serverKeyTwo = mtos.generateServerKey()
 
+var optionsAlice = {
+  username: 'alice',
+  passphrase: 'alice'
+}
+var optionsBob = {
+  username: 'bob',
+  passphrase: 'bob'
+}
+var optionsEve = {
+  username: 'eve',
+  passphrase: 'eve'
+}
+
+var userKeyAlice = mtos.newUserKey(optionsAlice)
+var userKeyBob = mtos.newUserKey(optionsBob)
+var userKeyEve = mtos.newUserKey(optionsEve)
+
 console.log('generating testing keys')
 
-Promise.all([serverKeyOne, serverKeyTwo])
-.then(function (serverKeys) {
+Promise.all([serverKeyOne, serverKeyTwo, userKeyAlice, userKeyBob, userKeyEve])
+.then(function (keys) {
   var promise = new Promise(function (resolve, reject) {
-    delete serverKeys[0].privateKey
-    delete serverKeys[0].publicKey
-    delete serverKeys[1].privateKey
-    delete serverKeys[1].publicKey
-    testingKeys.serverKeyOne = serverKeys[0]
-    testingKeys.serverKeyTwo = serverKeys[1]
+    for (var i = 0; i < keys.length; ++i) {
+      delete keys[i].privateKey
+      delete keys[i].publicKey
+    }
+
+    // Server Keys
+    testingKeys.serverKeyOne = keys[0]
+    testingKeys.serverKeyTwo = keys[1]
+
+    // User Keys
+    testingKeys.userKeyAlice = keys[2]
+    testingKeys.userKeyBob = keys[3]
+    testingKeys.userKeyEve = keys[4]
     fs.writeFileSync(path.join(__dirname + '/../test/testing-keys.json'),
                      JSON.stringify(testingKeys, null, 4), 'utf-8')
     resolve(testingKeys)
